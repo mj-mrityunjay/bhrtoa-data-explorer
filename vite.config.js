d9518c91b62bf386db36852c34aad8ef0dea2630
Mrityunjay Kumar <mks.mrityunjay@gmail.com>
2026-05-09 10:17:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/bhrtoa-data-explorer/', 
+  build: {
+    chunkSizeWarningLimit: 1500, // Increases the warning limit to 1.5 MB
+  }
 })
