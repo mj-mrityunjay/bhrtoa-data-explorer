2b44436c3837dfaef023feabc3c639365e981790
Mrityunjay Kumar <mks.mrityunjay@gmail.com>
2026-05-09 10:00:13 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ 
-  plugins: [react()], base: '/fm-dashboard/', 
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+export default defineConfig({
+  plugins: [react()],
+  base: '/bhrtoa-data-explorer/', 
 })
